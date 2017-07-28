593674c237a6592836d8e48e1618e8868104d196
unknown <xioli2013@gmail.com>
2017-07-28 11:01:14 -0600

    delete py files upload ProbSet4

---
+++ ProbSet4/gameSet_test/test_GetScore.py
@@ -0,0 +1,18 @@
+def test_GetScore():
+    """
+    Unit test for getWordScore
+    """
+    failure=False
+    # dictionary of words and scores
+    words = {("", 7):0, ("it", 7):4, ("was", 7):18, ("scored", 7):54, ("waybill", 7):155, ("outgnaw", 7):127, ("fork", 7):44, ("fork", 4):94}
+    for (word, n) in words.keys():
+        score = GetScore(word, n)
+        if score != words[(word, n)]:
+            print("FAILURE: test_GetScore()")
+            print("\tExpected", words[(word, n)], "points but got '" + str(score) + "' for word '" + word + "', n=" + str(n))
+            failure=True
+    if not failure:
+        print("SUCCESS: test_GetScore()")
+    
+    
+print(test_GetScore())
